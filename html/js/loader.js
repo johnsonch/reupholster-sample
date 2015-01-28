d94356be7ea6fd4254621e7686f899ea15e418f5
Chris Johnson <chris@johnsonch.com>
2015-01-27 20:09:34 -0600

    added loader script to clean up index.html

---
+++ html/js/loader.js
@@ -0,0 +1,12 @@
+function couchapp_load(scripts) {
+  for (var i=0; i < scripts.length; i++) {
+    document.write('<script src="'+scripts[i]+'"><\/script>')
+  };
+};
+
+couchapp_load([
+  "http://cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.min.js",
+  "http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/jquery-browser/0.0.7/jquery.browser.min.js",
+  "js/lib/jquery.couch.js"
+]);
