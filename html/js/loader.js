32189f350ad12f9107f95b42091b6da00c065e5f
Chris Johnson <chris@johnsonch.com>
2015-06-15 20:40:29 -0500

    bumped jquery and handlebars versions

--- html/js/loader.js
+++ html/js/loader.js
@@ -5,8 +5,8 @@ function couchapp_load(scripts) {
 };
 
 couchapp_load([
-  "http://cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.min.js",
-  "http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js",
+  "http://cdnjs.cloudflare.com/ajax/libs/handlebars.js/3.0.3/handlebars.min.js",
+  "http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/jquery-browser/0.0.7/jquery.browser.min.js",
   "js/lib/jquery.couch.js"
 ]);
